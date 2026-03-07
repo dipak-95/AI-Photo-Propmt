079e257e714d13ac44721c9e09ccd3633f0ee357
dipak-95 <dipakladani95@gmail.com>
2026-03-08 02:27:23 +0530

    Updated server API config version for APP version 4.0.0

--- admin/app/api/config/route.ts
+++ admin/app/api/config/route.ts
@@ -3,10 +3,11 @@ import { NextResponse } from 'next/server';
 export async function GET() {
     // Aap yahan se version control kar sakte hain
     return NextResponse.json({
-        latestVersion: '3.1',
-        minRequiredVersion: '1.0.0',
+        version: '4.1.0', // This will trigger update in app if app is < 4
+        latestVersion: '4.1.0',
+        minRequiredVersion: '4.0.0',
         updateUrl: 'https://play.google.com/store/apps/details?id=com.dipak.pearlai',
-        message: 'New features and better stability are available. Update now!',
-        forceUpdate: false // Agar true karoge to bina update kiye app nahi chalegi
+        message: 'A major update (v4.0.0) is available! New features and better stability. Update now!',
+        forceUpdate: false // Change to true if you want to block old versions
     });
 }
